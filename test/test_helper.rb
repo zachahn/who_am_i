692714ec8650f8dce617f53cbc379ff103234c84
Zach Ahn <engineering@zachahn.com>
2017-07-13 22:43:20 -0400

    Remove requires on the example models
    These models don't need an explicit require since this library searches
    for all the files which may need these annotations

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "remind_me"
 
 require "active_record"
-require_relative "support/models/application_record"
-require_relative "support/models/post"
-require_relative "support/models/category"
 require_relative "support/migrate"
 
 require "minitest/autorun"
