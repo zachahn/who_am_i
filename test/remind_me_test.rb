87b11d6b4bd0dab0db14f1678d83e3fac8111fc8
Zach Ahn <engineering@zachahn.com>
2017-09-04 15:54:41 -0400

    Remove test since it completely overlaps with other tests

--- test/remind_me_test.rb
+++ test/remind_me_test.rb
@@ -1,27 +1,4 @@
 require "test_helper"
 
 class RemindMeTest < TestCase
-  include Migrate
-
-  def test_it_prints_schema_for_one_table
-    migrate!
-
-    expected =
-      "# == Schema Info\n" \
-      "#\n" \
-      "# Table name: post\n" \
-      "#\n" \
-      "#   id             integer     not null, primary key   \n" \
-      "#   category_id    integer                             \n" \
-      "#   author         string      not null, default (Mr F)\n" \
-      "#   content        text                                \n" \
-      "#   created_at     datetime    not null                \n" \
-      "#   updated_at     datetime    not null                \n" \
-      "#\n"
-
-    engine = RemindMe::Comment.new(table_name: "post")
-    result = engine.output
-
-    assert_equal(expected, result)
-  end
 end
