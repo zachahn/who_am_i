e7023382f45163805f3ee2b2b89bf28ea11ae916
Zach Ahn <engineering@zachahn.com>
2017-09-04 11:15:04 -0400

    Add failing test for core functionality

--- test/remind_me_test.rb
+++ test/remind_me_test.rb
@@ -1,4 +1,27 @@
 require "test_helper"
 
 class RemindMeTest < TestCase
+  include Migrate
+
+  def test_it_prints_schema_for_one_table
+    migrate!
+
+    expected =
+      "# == Schema Info\n" \
+      "#\n" \
+      "# Table name: post\n" \
+      "#\n" \
+      "#   id          integer \n" \
+      "#   category_id integer \n" \
+      "#   author      string  \n" \
+      "#   content     text    \n" \
+      "#   created_at  datetime\n" \
+      "#   updated_at  datetime\n" \
+      "#\n"
+
+    engine = RemindMe::Comment.new(table_name: "post")
+    result = engine.output
+
+    assert_equal(expected, result)
+  end
 end
