fce90bf9114ee6e437d44150f6969c70c0e21ffa
Zach Ahn <engineering@zachahn.com>
2017-12-31 10:15:34 +0100

    Update user-facing rake tasks
    My original idea was to have multiple rake tasks corresponding to a
    category of files, for example `who_am_i:models` and
    `who_am_i:factories`.
    
    However, I ended up realizing that moving that stuff into a
    configuration file would be simpler. (What would happen if configuration
    file said "annotate fixtures, don't annotate models", but
    `who_am_i:fixtures` was run?)
    
    This commit updates the rake tasks to match the new behavior.

--- lib/who_am_i/rake.rb
+++ lib/who_am_i/rake.rb
@@ -1,6 +1,6 @@
 namespace :who_am_i do
   desc "Annotate models"
-  task :models do
+  task :main do
     if !ActiveRecord::Base.connected?
       # Do something
     end
@@ -8,9 +8,7 @@ namespace :who_am_i do
     main = WhoAmI::Function::Main.new(File.expand_path("."))
     main.call
   end
-
-  task all: [:models]
 end
 
 desc "Annotate"
-task who_am_i: "who_am_i:all"
+task who_am_i: "who_am_i:main"
