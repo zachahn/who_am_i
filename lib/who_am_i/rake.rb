bef771690fa43d83794326b4c2b8bc8f97e31118
Zach Ahn <engineering@zachahn.com>
2018-01-02 14:29:14 +0100

    Update user-facing rake

--- lib/who_am_i/rake.rb
+++ lib/who_am_i/rake.rb
@@ -1,9 +1,7 @@
 namespace :who_am_i do
   desc "Annotate models"
   task :main do
-    if !ActiveRecord::Base.connected?
-      # Do something
-    end
+    require "who_am_i"
 
     main = WhoAmI::Function::Main.new(File.expand_path("."))
     main.call
